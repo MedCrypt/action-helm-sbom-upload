7848d4d6180c61a9d46759360a0767f0b72dd0c3
Matt Burnside <matt@medcrypt.co>
2024-05-06 16:35:24 -0500

    Revert "Changed linter config to json, working through some linting issues"
    This reverts commit a22e423f8f59958f5b72e248f3232dd70c9089af.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-import core from '@actions/core';
-
+import * as core from '@actions/core';
 import { ListOrganizations, OrganizationInfo } from './protobuf/v1/external/heim_organization_pb';
 import {
   CreateOrganizationProduct,
@@ -11,7 +10,7 @@ import {
 import { unparse } from 'uuid-parse';
 import { UUID } from './protobuf/heim_common_pb';
 import { OrganizationProductVersion } from './protobuf/heim_organization_product_pb';
-import { readFile } from 'node:fs/promises';
+import { readFile, stat } from 'node:fs/promises';
 import { SubmitSbom } from './protobuf/v1/external/heim_sbom_pb';
 import { existsSync } from 'node:fs';
 import { basename } from 'node:path';
@@ -43,6 +42,7 @@ export async function run(): Promise<void> {
     required: true,
     trimWhitespace: true,
   };
+  console.log(`This is the version with protobuf stripped out.`);
   let baseUrl: string = core.getInput('api-base-url', inputOptions);
   if (!baseUrl.endsWith('/')) {
     baseUrl += '/';
@@ -66,34 +66,35 @@ export async function run(): Promise<void> {
   }
   const fileName = basename(sbomFilePath);
   const fileReadResults = await readFile(sbomFilePath);
-  core.info(`The SBOM file is ${fileReadResults.byteLength} bytes.`);
+
+  console.log(`The SBOM file is ${fileReadResults.byteLength} bytes.`);
 
   const defaultOrg = await GetDefaultOrganization(callInfo);
-  if (defaultOrg === undefined) {
+  if (defaultOrg == undefined) {
     core.setFailed(`Unable to determine default organization`);
     return;
   }
   const orgUuid = defaultOrg.getOrg()?.getId();
 
   const allProducts = await ListAllProducts(orgUuid, callInfo);
-  core.info(`Resolving product (${productName}) and version (${productVersionName})...`);
-  const foundProducts = allProducts.filter((p) => p.getName().toLowerCase() === productName.toLowerCase());
+  console.log(`Resolving product (${productName}) and version (${productVersionName})...`);
+  const foundProducts = allProducts.filter((p) => p.getName().toLowerCase() == productName.toLowerCase());
   let foundOrCreatedProduct: OrganizationProduct | undefined = undefined;
-  if (foundProducts.length === 0) {
+  if (foundProducts.length == 0) {
     if (!shouldCreate) {
       core.setFailed(`Unable to locate product ${productName}, and create-product-and-version-if-missing is false.`);
       return;
     }
-    core.info(`Creating product ${productName}...`);
+    console.log(`Creating product ${productName}...`);
     foundOrCreatedProduct = await CreateProduct(orgUuid, productName, callInfo);
   } else {
-    core.info(`Found existing product ${productName}`);
+    console.log(`Found existing product ${productName}`);
     foundOrCreatedProduct = foundProducts[0];
   }
 
   const allVersions = await ListAllVersionsOfProduct(foundOrCreatedProduct.getId(), callInfo);
   const foundVersions = allVersions.filter(
-    (v) => v.getRawVersionString().toLowerCase() === productVersionName.toLowerCase(),
+    (v) => v.getRawVersionString().toLowerCase() == productVersionName.toLowerCase(),
   );
   let foundOrCreatedVersion: OrganizationProductVersion | undefined = undefined;
   if (foundVersions.length == 0) {
@@ -103,18 +104,18 @@ export async function run(): Promise<void> {
       );
       return;
     }
-    core.info(`Creating version ${productVersionName} for product ${productName}...`);
+    console.log(`Creating version ${productVersionName} for product ${productName}...`);
     foundOrCreatedVersion = await CreateProductVersion(foundOrCreatedProduct.getId(), productVersionName, callInfo);
   } else {
-    core.info(`Found existing version ${productVersionName}`);
+    console.log(`Found existing version ${productVersionName}`);
     foundOrCreatedVersion = foundVersions[0];
   }
 
-  core.info(`Uploading SBOM...`);
+  console.log(`Uploading SBOM...`);
   // finally, upload the SBOM
   const sbomUploadResponse = await UploadSbom(foundOrCreatedVersion.getId(), fileName, fileReadResults, callInfo);
-  core.info(`SBOM uploaded successfully.`);
-  sbomUploadResponse.statusMessages.forEach((msg) => core.info(`Response Message: ${msg}`));
+  console.log(`SBOM uploaded successfully.`);
+  sbomUploadResponse.statusMessages.forEach((msg) => console.log(`Response Message: ${msg}`));
 }
 
 // overloaded function that lets TS enforce if an undefined cant be passed in, an undefined won't be returned
@@ -137,7 +138,7 @@ const GetDefaultOrganization = async (callInfo: ApiCallInformation): Promise<Org
   listOrganizations.setRequest(request);
   const orgResponse = await DoWebApiPostRequest('listorganizations', listOrganizations, ListOrganizations, callInfo);
   const orgs = orgResponse.getResponse()?.getOrginfoList();
-  if (orgs === undefined || orgs.length === 0) {
+  if (orgs == undefined || orgs.length == 0) {
     throw Error(`Unable to determine default organization out of ${orgs?.length} possible candidates`);
   }
   return orgs[0];
@@ -216,7 +217,7 @@ const CreateProductVersion = async (
   versionString: string,
   callInfo: ApiCallInformation,
 ): Promise<OrganizationProductVersion> => {
-  core.info(`Creating version ${versionString} for product...`);
+  console.log(`Creating version ${versionString} for product...`);
   const createVersion = new CreateOrganizationProductVersion();
   const requestData = new CreateOrganizationProductVersion.Request();
   createVersion.setRequest(requestData);
