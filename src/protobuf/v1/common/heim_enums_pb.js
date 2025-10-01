b76ce13636f23fe105ce5aa6993cbf173e1ca48b
Haritha <haritha@medcrypt.co>
2025-09-30 21:28:30 -0400

    Few changes we may use in the future when needed

--- src/protobuf/v1/common/heim_enums_pb.js
+++ src/protobuf/v1/common/heim_enums_pb.js
@@ -11,10 +11,15 @@ var jspb = require('google-protobuf');
 var goog = jspb;
 var global = Function('return this')();
 
+goog.exportSymbol('proto.medcrypt.helm.web.enums.AccessLevel', null, global);
 goog.exportSymbol('proto.medcrypt.helm.web.enums.FileFormat', null, global);
 goog.exportSymbol('proto.medcrypt.helm.web.enums.ProductMatchType', null, global);
 goog.exportSymbol('proto.medcrypt.helm.web.enums.ReportRequestState', null, global);
 goog.exportSymbol('proto.medcrypt.helm.web.enums.ReviewStatus', null, global);
+goog.exportSymbol('proto.medcrypt.helm.web.enums.RuleCondition', null, global);
+goog.exportSymbol('proto.medcrypt.helm.web.enums.RuleInputField', null, global);
+goog.exportSymbol('proto.medcrypt.helm.web.enums.RuleOutputCondition', null, global);
+goog.exportSymbol('proto.medcrypt.helm.web.enums.RuleOutputField', null, global);
 /**
  * @enum {number}
  */
@@ -64,6 +69,58 @@ proto.medcrypt.helm.web.enums.FileFormat = {
   FF_HTML: 10,
   FF_YAML: 11,
   FF_SPDX: 12
+};
+
+/**
+ * @enum {number}
+ */
+proto.medcrypt.helm.web.enums.RuleCondition = {
+  NOT_SET: 0,
+  EXACT: 1,
+  GREATER_THAN: 2,
+  LESS_THAN: 3,
+  CONTAINS: 4,
+  STARTS_WITH: 5,
+  ENDS_WITH: 6
+};
+
+/**
+ * @enum {number}
+ */
+proto.medcrypt.helm.web.enums.RuleInputField = {
+  NO_INPUT_SET: 0,
+  COMPONENT_NAME: 1,
+  COMPONENT_SUPPLIER: 2,
+  COMPONENT_VERSION: 3,
+  CPE: 4,
+  PURL: 5
+};
+
+/**
+ * @enum {number}
+ */
+proto.medcrypt.helm.web.enums.RuleOutputField = {
+  NO_OUTPUT_SET: 0,
+  EOL_EOS: 1,
+  LEVEL_OF_SUPPORT: 2,
+  IR_PRODUCT_NAME: 5
+};
+
+/**
+ * @enum {number}
+ */
+proto.medcrypt.helm.web.enums.RuleOutputCondition = {
+  DATE: 0,
+  TEXT: 1
+};
+
+/**
+ * @enum {number}
+ */
+proto.medcrypt.helm.web.enums.AccessLevel = {
+  NO_ACCESS: 0,
+  VIEW_ONLY: 1,
+  MODIFY: 2
 };
 
 goog.object.extend(exports, proto.medcrypt.helm.web.enums);
